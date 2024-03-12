807844b4194a62c603a982c826d69a3884f3742d
DomenicoCicero <domenico.cicero8@gmail.com>
2024-03-13 00:57:17 +0100

    add-state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MyNav from "./components/MyNav";
 import MyFooter from "./components/MyFooter";
 import Welcome from "./components/Welcome";
-import AllTheBooks from "./components/AllTheBooks";
+// import AllTheBooks from "./components/AllTheBooks";
+import Books from "./components/Books";
 
 function App() {
   return (
     <div>
       <MyNav />
       <Welcome />
-      <AllTheBooks image="image" card="card" button="button" />
+      <Books image="image" card="card" button="button" />
+      {/* <AllTheBooks image="image" card="card" button="button" /> */}
       <MyFooter />
     </div>
   );
